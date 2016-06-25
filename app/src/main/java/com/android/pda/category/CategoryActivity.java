adfae64d82bd6e256ae4b7cb477c6591b880ab7e
vaibhavi <vaibhavi9.rana@gmail.com>
2016-06-25 16:51:09 +0530

    fgfdghfd

---
+++ app/src/main/java/com/android/pda/category/CategoryActivity.java
@@ -0,0 +1,22 @@
+package com.android.pda.category;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.android.pda.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_category);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/android/pda/category/CategoryPojo.java
@@ -0,0 +1,12 @@
+package com.android.pda.category;
+
+/**
+ * Created by vraj on 6/25/2016.
+ */
+
+public class CategoryPojo {
+
+    public int catId;
+    public String catName;
+
+}
